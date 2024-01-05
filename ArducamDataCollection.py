73708d042d86eac6a12ecdf5b1d90feaa7dd15e6
abhilash-sunkara <52720913+abhilash-sunkara@users.noreply.github.com>
2024-01-05 13:51:08 -0600

    Automated collection of multiple images

--- ArducamDataCollection.py
+++ ArducamDataCollection.py
@@ -1,11 +1,13 @@
 import time
-import  threading
+import threading
 from picamera2 import Picamera2, Preview
+
 
 class Camera(object):
     # frame_reader = None
     cam = None
     _value_lock = None
+
     # previewer = None
 
     def __init__(self, width=640, height=360):
@@ -14,35 +16,44 @@ class Camera(object):
 
     def open_camera(self, width=640, height=360, framerate=30):
         self.cam = Picamera2()
-        self.cam.configure(self.cam.create_preview_configuration(main={"size":(width, height)},buffer_count=4))
+        self.cam.configure(self.cam.create_preview_configuration(main={"size": (width, height)}, buffer_count=4))
         # self.cam.start_preview(Preview.QTGL)
         # self.cam.start()
 
-    def getFrame(self,a_wait: bool = True):
+    def getFrame(self, a_wait: bool = True):
         with self._value_lock:
             return self.cam.capture_array(wait=a_wait)
 
-    def start_preview(self,a_preview:bool = False):
+    def start_preview(self, a_preview: bool = False):
         if a_preview == True:
             self.cam.start_preview(Preview.QTGL)
-        else :
+        else:
             self.cam.start_preview()
         self.cam.start()
 
     def stop_preview(self):
         self.cam.stop_preview()
         self.cam.stop()
-    
+
     def close(self):
         self.cam.close()
+
+    def takeImage(self, imgString):
+        capture_config = camera.cam.create_still_configuration()
+        self.cam.switch_mode_and_capture_file(capture_config, imgString)
+
+
+def getImgString(num):
+    return "image" + num + ".jpg"
+
 
 if __name__ == "__main__":
     camera = Camera()
     camera.start_preview(True)
-    capture_config = camera.cam.create_still_configuration()
-    time.sleep(5)
-    camera.cam.switch_mode_and_capture_file(capture_config, "image.jpg")
+
+    for i in range(5):
+        time.sleep(2)
+        camera.takeImage(getImgString(i))
+
     camera.stop_preview()
     camera.close()
-
-        
